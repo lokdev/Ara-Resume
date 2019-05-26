d6a9d790255b69fe08844500d1f2ac0bf2008481
Lokesh <lokdev@gmail.com>
2019-05-26 12:57:05 -0500

    Updates

--- precache-manifest.db0159c7825d7d37c899d6daf698b01e.js
+++ precache-manifest.db0159c7825d7d37c899d6daf698b01e.js
@@ -12,15 +12,15 @@ self.__precacheManifest = [
     "url": "/Ara-Resume/static/js/runtime~main.bdc3c081.js"
   },
   {
-    "revision": "52be18b6fa7645895235",
-    "url": "/Ara-Resume/static/js/main.57e75caa.chunk.js"
+    "revision": "7431a1aadd4556f95d0c",
+    "url": "/Ara-Resume/static/js/main.3ecc25b5.chunk.js"
   },
   {
     "revision": "ad956c0db332ba7e38e6",
     "url": "/Ara-Resume/static/js/2.af71e73e.chunk.js"
   },
   {
-    "revision": "52be18b6fa7645895235",
+    "revision": "7431a1aadd4556f95d0c",
     "url": "/Ara-Resume/static/css/main.a8ae3a1e.chunk.css"
   },
   {
@@ -28,7 +28,7 @@ self.__precacheManifest = [
     "url": "/Ara-Resume/static/css/2.4e4f478d.chunk.css"
   },
   {
-    "revision": "a66c27dad26f6aa0e2a7178e1f1f2a72",
+    "revision": "1e9937b6446b3de24502475556a6eef7",
     "url": "/Ara-Resume/index.html"
   }
 ];
